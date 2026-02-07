5a455c909c30811dcc4013c0188a8bc54a271ffd
Rose Irungu <127296273+Rose-Irungu@users.noreply.github.com>
2026-02-07 09:59:26 +0300

    Fix CORS for frontend access

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "App is running 🚀"
+CORS(
+    app,
+    resources={r"/*": {"origins": "*"}},
+    supports_credentials=True
+)
